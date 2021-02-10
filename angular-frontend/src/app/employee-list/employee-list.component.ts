6030a6040520241b73e1d51269c30d160c38aa5e
Guilherme Henrique Ribeiro <guilherme.h.ribeiro@bradesco.com.br>
2021-02-09 23:12:31 -0300

    delete e view employee

--- angular-frontend/src/app/employee-list/employee-list.component.ts
+++ angular-frontend/src/app/employee-list/employee-list.component.ts
@@ -30,4 +30,15 @@ export class EmployeeListComponent implements OnInit {
     this.router.navigate(['update-employee', id]);
   }
 
+  deleteEmployee(id: number) {
+    this.employeeService.deleteEmployee(id).subscribe(data => {
+      console.log(data);
+      this.getEmployees();
+    });
+  }
+
+  employeeDetails(id: number) {
+    this.router.navigate(['employee-details', id]);
+  }
+
 }
